7c80163186623a4f151cd31e9b33bd29f9add523
DunsinK <133840905+DunsinK@users.noreply.github.com>
2025-01-25 15:31:42 -0600

    OAuth Works
    OAuth works only on localhost 5173

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,34 +2,37 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {GoogleLogin} from 'react-google-login'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const responseGoogle = response =>{
+    console.log(response)
+  }
+  const responseError = error =>{
+    console.log(error)
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div tag = "OAuthAndSignIn">
+        <button onClick={OAuthAndSignIn}>Sign In</button>
+        <GoogleLogin 
+          clientId='403097048891-enf7qgq6bp95hjhjhsmulkvltsde7p14.apps.googleusercontent.com'
+          buttonText = 'Sign in & Authorize Calender'
+          onSuccess={responseGoogle}
+          onFailure={responseError}
+          cookiePolicy={'single_host_origin'}
+          // This is important
+          responseType='code'
+          accessType='offline'
+          scope ='openid email profile https://www.googleapis.com/auth/calendar' // have to ad scope 
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
-
+function OAuthAndSignIn(){
+  console.log("OAuth")
+}
 export default App
